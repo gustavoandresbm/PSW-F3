b0234ef5a62d4fe5563c9f124d329cdc56334063
gustavoandresbm <gustavoandresbm@gmail.com>
2022-10-25 15:57:23 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,22 @@
-function calcularventa(){
-            
-    var a = parseFloat(document.f1.getElementById('vc').value);
-    var b = parseFloat(document.f1.getElementById('put').value);
-    var b = parseFloat(document.f1.getElementById('iva').value);
-
-    vv=(a)+((a)*((b)/100))+((a)*((c)/100));
-    uv=((a)*((b)/100))
-
-    document.f1.getElementById('vvf').value=vv;
-
-    document.f1.getElementById('un').value=uv;
-
-}        
-
-function calculardatos(){
-            
+function calcularventa() {
+    var a = parseFloat(document.getElementById('vc').value);
+    var b = parseFloat(document.getElementById('put').value);
+    var c = parseFloat(document.getElementById('iva').value);
+  
+    vv = a + a * (b / 100) + a * (c / 100);
+    uv = a * (b / 100);
+  
+    document.getElementById('vvf').value = vv;
+  
+    document.getElementById('un').value = uv;
+  }
+  
+  function calculardatos() {
     var a = document.cg.vc.value;
     var b = document.cg.opc1.value;
     var c = document.cg.opc2.value;
     var r = document.cg.conv.value;
-    r=(parseFloat(a)*parseFloat(c))/parseFloat(b);
-    document.cg.conv.value=r;
-    
-}
+    r = (parseFloat(a) * parseFloat(c)) / parseFloat(b);
+    document.cg.conv.value = r;
+  }
+  
